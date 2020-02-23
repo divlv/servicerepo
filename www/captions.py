270c2b2d40f221638e4386175e9f3d2892125f45
dimedrol <mail@me.com>
2020-02-23 20:16:45 +0200

    Updated layout; New search field; captions

--- www/captions.py
+++ www/captions.py
@@ -8,6 +8,21 @@ project_cip="ServiceRepo"
 
 nav_up="To the top"
 
+cap_findsrv="Find service"
+cap_findsrv_desc="Find service by JSON key"
+cap_findsrvtag_desc="...or by query variable, tag etc."
+cap_injson="Incoming JSON"
+cap_outjson="Incoming JSON"
+cap_search_and="AND"
+cap_search_or="OR"
+cap_showall_conf="Show all saved services?"
+#
+b_find="Find service"
+b_findall="Show all"
+#
+#
+err_emptyq="Empty search query"
+
 ###
 menu_main="Home"
 menu_news="Новости"
